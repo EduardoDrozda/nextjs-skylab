4e6d7e1c353b1ea80f92fed7a25205fcc6db0f03
Eduardo  Drozda <eduardodrozda@agenciacodigo.com>
2020-08-20 08:09:59 -0300

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import ThemeContainer from "../context/theme/ThemeContainer"
+
+function MyApp({ Component, pageProps }) {
+  return <ThemeContainer>
+    <Component {...pageProps} />
+  </ThemeContainer>
+}
+
+export default MyApp
